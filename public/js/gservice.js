8111d05ed70be004a323d58d9203bd4b35d503a0
John Park <jwpark417@gmail.com>
2016-06-27 14:59:21 -0400

    Add gservice and refresh() to rest of app

--- public/js/gservice.js
+++ public/js/gservice.js
@@ -55,7 +55,7 @@ angular.module('gservice', [])
 
         // Converts each of the JSON records into Google Maps location format (Note: [Lat, Lng] format).
         locations.push({
-          latlong: new google.maps.LatLng(user.location[1], user.location[0]),
+          latlon: new google.maps.LatLng(user.location[1], user.location[0]),
           message: new google.maps.InfoWindow({
             content: contentString,
             maxWidth: 320
